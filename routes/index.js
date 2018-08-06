d7231390117fb8594a3a40b53dd9aac2f9ff91ec
Jason Morton <info@webbiz.ie>
2018-08-06 17:48:46 +0300

    First app on AWS

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* POST*/
+router.post('/test', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.status(200).send({message: 'connecting'});
+});
+
+module.exports = router;
