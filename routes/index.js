5f97886b9a2b840432958a88f6c31c1dff8ce09f
Jason Morton <info@webbiz.ie>
2018-08-07 10:27:09 +0300

    Change cors settings

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 /* POST*/
 router.post('/test', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.status(200).send({message: 'connecting'});
+  res.status(200).send({message: 'connecting S3 CORS enabled'});
 });
 
 module.exports = router;
